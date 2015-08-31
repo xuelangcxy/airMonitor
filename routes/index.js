957e2f00da6f8aa385d78a5928c6b024cf7f3795
chengxueyong <1361272604@qq.com>
2015-08-31 15:54:38 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'weather' });
+});
+
+router.get('/realtime', function(req, res, next) {
+  res.render('realtime', { title: 'air' });
+});
+
+module.exports = router;
